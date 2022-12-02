5321650b3106b257b8ba2e89bae70cf21f2bf5ce
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2022-12-03 01:56:05 +0900

    modify ridiculous code

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -9,16 +9,15 @@ export class SearchService {
     // @InjectDataSource(dataSource)
     // private datasource,
 
-    private connectionService: ConnectionService
+    private connectionService: ConnectionService,
   ) {}
 
   async getAll(
     division: number,
     address: number,
     language: number,
-    priority: number,
-  )//: Promise<[]> 
-  {
+    priority: number, //: Promise<[]>
+  ) {
     const divisions = [
       'Naes',
       'Woes',
@@ -69,97 +68,97 @@ export class SearchService {
 
     const part = divisions[Number(division)];
     const province = addressArray[Number(address)];
-    const today = new Date()
-    const dayName = today.toDateString().toLowerCase().split(" ")[0];
-    const timeNow = today.toTimeString().toLowerCase().split(":")[0];
+    const today = new Date();
+    const dayName = today.toDateString().toLowerCase().split(' ')[0];
+    const timeNow = today.toTimeString().toLowerCase().split(':')[0];
 
     /**
      * 위치우선 : 위치와 시간조건으로 검색합니다.
      * 해당 시간에 운영중인 병원이 없으면 (새벽의 경우) 그 구역의 응급실 운영 병원을 검색합니다.
      */
-    switch(priority) {
+    switch (priority) {
       case 1:
-      //인덱스를 타지 않는 쿼리
-      // const hospitals = await this.datasource.manager.query(`SELECT * FROM ` + part + ` WHERE address Like ? AND ` + dayName + ` IS NOT NULL AND SUBSTRING_INDEX(`+dayName+`, ':', 1) < ? `,
-      // [ `%${province}%`, timeNow ])
+        //인덱스를 타지 않는 쿼리
+        // const hospitals = await this.datasource.manager.query(`SELECT * FROM ` + part + ` WHERE address Like ? AND ` + dayName + ` IS NOT NULL AND SUBSTRING_INDEX(`+dayName+`, ':', 1) < ? `,
+        // [ `%${province}%`, timeNow ])
 
-      //풀텍스트 인덱스를 타는 쿼리 
-      const [hospitals, fields] = await this.connectionService.connection.query(
-        `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part + ` WHERE MATCH(address) AGAINST(?) AND ` + dayName + ` IS NOT NULL AND (? BETWEEN SUBSTRING_INDEX(`+dayName+`, ':', 1) AND (SUBSTRING(`+dayName+`, 7, 2) + 1)) `, 
-        [province, timeNow],
-      );
-
-      if (Array.isArray(hospitals) && hospitals.length !== 0) {
-        return hospitals;
-
-      } else if (Array.isArray(hospitals) && hospitals.length === 0) {
-        const [hospitals, fields] = await this.connectionService.connection.query(
-          `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM Emergencies WHERE MATCH(address) AGAINST(?)`,
-          [province],
-        );
-        // const hospitals = await this.datasource.manager.query(
-        //   `SELECT * FROM Emergencies WHERE address Like ?`,
-        //   [`%${province}%`],
-        // );
-        return hospitals;
-      }
-    }
-
-    /**
-     * 언어우선 : 언어와 위치 조건으로 검색합니다.
-     * 해당 구역에 해당 언어로 진료하는 병원이 없으면, 서울시로 넓혀 검색합니다.
-     * 그래도 해당 언어로 진료하는 병원이 없으면, 영어진료가 가능한 병원으로 응답합니다.
-     */
-    switch (priority) {
-      case 2:
-        
-      const [hospitals, fields] = await this.connectionService.connection.query(
-        `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part + ` WHERE ((MATCH(address) AGAINST(?)) AND SUBSTRING(foreignLanguages, ?, 1) LIKE 1)`,
-        [ province, Number(language) ]
-      );
-      // const hospitals = await this.datasource.manager.query(
-      //   `SELECT * FROM ` + part + ` WHERE ((address Like ?) AND SUBSTRING(foreignLanguages, ?, 1) LIKE 1)`,
-      //   [ `%${province}%`, Number(language) ]
-      // );
-
-      if (Array.isArray(hospitals) && hospitals.length !== 0) {
-        return hospitals;
-
-      } else if (Array.isArray(hospitals) && hospitals.length === 0) {
-        const [hospitals, fields] = await this.connectionService.connection.query(
-          `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part + ` WHERE SUBSTRING(foreignLanguages, ?, 1) LIKE 1`,
-          [ Number(language) ]
-        );
-
-        if (Array.isArray(hospitals) && hospitals.length !== 0) {
-          return hospitals;
-
-        } else if (Array.isArray(hospitals) && hospitals.length === 0) {
-          const [hospitals, fields] = await this.connectionService.connection.query(
-            `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part + ` WHERE ((MATCH(address) AGAINST(?)) AND SUBSTRING(foreignLanguages, 1, 1) LIKE 1)`,
-            [ province ]
+        //풀텍스트 인덱스를 타는 쿼리
+        const [localHospital, localFields] =
+          await this.connectionService.connection.query(
+            `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part +` WHERE MATCH(address) AGAINST(?) AND ` + dayName + ` IS NOT NULL AND (? BETWEEN SUBSTRING_INDEX(` + dayName + `, ':', 1) AND (SUBSTRING(` + dayName + `, 7, 2) + 1)) `,
+            [province, timeNow],
           );
+
+        if (Array.isArray(localHospital) && localHospital.length !== 0) {
+          return localHospital;
+
+        } else if (Array.isArray(localHospital) && localHospital.length === 0) {
+          const [localHospital, localFields] =
+            await this.connectionService.connection.query(
+              `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM Emergencies WHERE MATCH(address) AGAINST(?)`,
+              [province],
+            );
           // const hospitals = await this.datasource.manager.query(
-          //   `SELECT * FROM ` + part + ` WHERE ((address Like ?) AND SUBSTRING(foreignLanguages, 1, 1) LIKE 1)`,
-          //   [ `%${province}%` ]
+          //   `SELECT * FROM Emergencies WHERE address Like ?`,
+          //   [`%${province}%`],
           // );
-          return hospitals;
+          return localHospital;
         }
-      }
-    }
+        break;
 
-    /**
-     * 응급실 우선
-     * 해당 지역의 응급실 운영 병원을 불러옵니다.
-     */
-    switch (priority) {
+      /**
+       * 언어우선 : 언어와 위치 조건으로 검색합니다.
+       * 해당 구역에 해당 언어로 진료하는 병원이 없으면, 서울시로 넓혀 검색합니다.
+       * 그래도 해당 언어로 진료하는 병원이 없으면, 영어진료가 가능한 병원으로 응답합니다.
+       */
+      case 2:
+        const [localMotherTongueHospital, localLanguageFields] = await this.connectionService.connection.query(
+            `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part + ` WHERE ((MATCH(address) AGAINST(?)) AND SUBSTRING(foreignLanguages, ?, 1) LIKE 1)`,
+            [province, Number(language)],
+          );
+        // const hospitals = await this.datasource.manager.query(
+        //   `SELECT * FROM ` + part + ` WHERE ((address Like ?) AND SUBSTRING(foreignLanguages, ?, 1) LIKE 1)`,
+        //   [ `%${province}%`, Number(language) ]
+        // );
+        switch (true) {
+          case Array.isArray(localMotherTongueHospital) && localMotherTongueHospital.length !== 0:
+            return localMotherTongueHospital;
+
+          case Array.isArray(localMotherTongueHospital) && localMotherTongueHospital.length === 0:
+            const [seoulMotherTongueHospital, seoulLanguageFeilds] = await this.connectionService.connection.query(
+                `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part + ` WHERE SUBSTRING(foreignLanguages, ?, 1) LIKE 1`,
+                [Number(language)],
+              );
+
+            if (Array.isArray(seoulMotherTongueHospital) && seoulMotherTongueHospital.length !== 0) {
+              return seoulMotherTongueHospital;
+
+            } else if (Array.isArray(seoulMotherTongueHospital) && seoulMotherTongueHospital.length === 0) {
+              const [englishHospital, englishLanguageFields] = await this.connectionService.connection.query(
+                `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM ` + part + ` WHERE ((MATCH(address) AGAINST(?)) AND SUBSTRING(foreignLanguages, 1, 1) LIKE 1)`,
+                  [province],
+                );
+              // const hospitals = await this.datasource.manager.query(
+              //   `SELECT * FROM ` + part + ` WHERE ((address Like ?) AND SUBSTRING(foreignLanguages, 1, 1) LIKE 1)`,
+              //   [ `%${province}%` ]
+              // );
+              return englishHospital;
+            }
+        }
+        break;
+
+      /**
+       * 응급실 우선
+       * 해당 지역의 응급실 운영 병원을 불러옵니다.
+       */
+
       case 3:
-
-      const [hospitals, fields] = await this.connectionService.connection.query(
-        `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM Emergencies WHERE MATCH(address) AGAINST(?)`,
-        [ province ]
-      );
-      return hospitals;
+        const [emergenyHospital, emergencyFields] =
+          await this.connectionService.connection.query(
+            `SELECT hospitalName, hospitalSize, phoneNumber, address, mon, tue, wed, thu, fri, sat, sun, holiday, foreignLanguages FROM Emergencies WHERE MATCH(address) AGAINST(?)`,
+            [province],
+          );
+        return emergenyHospital;
       // const hospitals = await this.datasource.manager.query(
       //   `SELECT * FROM Emergencies WHERE address Like ?`,
       //   [ `%${province}%` ]
@@ -167,6 +166,4 @@ export class SearchService {
       //return hospitals;
     }
   }
-
-
 }
